fa212f20c931cd2639bb04e14650bd5c8b378794
Michael <michael.steurer@me.com>
2016-06-05 14:19:15 +0200

    orbit

---
+++ orbit/script/main.js
@@ -0,0 +1,18 @@
+function clear(){
+    if ($('form .placeholder').val() == 'hello@twysvn.com') {
+        $('form .placeholder').val('');
+        $('form .placeholder').css('color', 'rgba(255, 255, 255, 1)');
+    }
+}
+
+function reset(){
+    if ($('form .placeholder').val().length <= 0) {
+        $('form .placeholder').val('hello@twysvn.com');
+        $('form .placeholder').css('color', 'rgba(255, 255, 255, 0.4)');
+    }
+}
+
+$(document).ready(function(){
+    document.getElementById('input').addEventListener("focus", clear);
+    document.getElementById('input').addEventListener("blur", reset);
+});
